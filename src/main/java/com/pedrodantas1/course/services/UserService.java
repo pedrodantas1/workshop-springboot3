cc79b6bb91a446f2d05ca434a79418dff9102ea4
Pedro Dantas <pedrood1232@gmail.com>
2022-10-04 10:19:45 -0300

    Created service layer and component registration

---
+++ src/main/java/com/pedrodantas1/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.pedrodantas1.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedrodantas1.course.entities.User;
+import com.pedrodantas1.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
